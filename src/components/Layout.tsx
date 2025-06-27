5d77f67308550e699c4ae8896fe74caab7cb8a0e
Marco B <marco.b@intrapresa-it.it>
2025-06-27 17:03:10 +0200

    layout fix + import public images

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,51 +1,36 @@
-
-import "../index.css"; 
+// src/components/Layout.tsx
+import React from "react";
+import "../index.css";
 import { Toaster } from "../components/ui/toaster";
 import { SidebarProvider } from "../components/ui/sidebar";
 import AppSidebar from "../components/layout/app-sidebar";
 import AppHeader from "../components/layout/app-header";
 import { TooltipProvider } from "../components/ui/tooltip";
 
-
-export default function Layout({
-  children,
-}: {
+interface LayoutProps {
   children: React.ReactNode;
-}) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body className="font-body antialiased">
-        {/* wrap everything in your provider */}
-        <SidebarProvider defaultOpen={true}>
-          <TooltipProvider delayDuration={0}>
-            <div className="flex min-h-screen">
-              {/* this renders your mobile & desktop sidebar */}
-              <AppSidebar />
+}
 
-              {/* main area */}
-              <div className="flex flex-1 flex-col">
-                <AppHeader />
-                <main className="flex flex-col flex-1 w-full p-4 md:p-6 lg:p-8 overflow-y-auto">
-                  {children}
-                </main>
-              </div>
-            </div>
-            <Toaster />
-          </TooltipProvider>
-        </SidebarProvider>
-      </body>
-    </html>
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <SidebarProvider defaultOpen={true}>
+      <TooltipProvider delayDuration={0}>
+        <div className="flex min-h-screen font-body antialiased bg-page-bg">
+          {/* Sidebar */}
+          <AppSidebar />
+
+          {/* Main content */}
+          <div className="flex flex-1 flex-col">
+            <AppHeader />
+            <main className="flex flex-col flex-1 w-full p-4 md:p-6 lg:p-8 overflow-y-auto">
+              {children}
+            </main>
+          </div>
+        </div>
+
+        {/* Toast notifications */}
+        <Toaster />
+      </TooltipProvider>
+    </SidebarProvider>
   );
 }
